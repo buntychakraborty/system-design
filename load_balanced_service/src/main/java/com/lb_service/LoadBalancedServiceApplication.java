9abe0a3593a75a2a250ec4f0c84875b6443c336c
buntychakraborty <buntychakraborty@outlook.com>
2021-06-10 00:27:14 +0530

    Lb service added

---
+++ load_balanced_service/src/main/java/com/lb_service/LoadBalancedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lb_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancedServiceApplication.class, args);
+	}
+
+}
---
+++ load_balanced_service/src/main/java/com/lb_service/controller/LBServiceController.java
@@ -0,0 +1,18 @@
+package com.lb_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LBServiceController {
+	
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping("/app")
+	public String getData() {
+		return "Getting response from "+port;
+	}
+
+}
